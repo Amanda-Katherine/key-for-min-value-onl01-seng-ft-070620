24070e0e1eb8aff66e11a8d1fc12b0c22496acbe
Amanda Walker <walker.akw@gmail.com>
2020-07-20 05:59:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,8 @@
 #example hask below
 #jerseys = {:Amanda => 21, :Daniel => 3, :Becky => 23}
 
+require 'pry'
+
 def key_for_min_value(jerseys)
   min_name = nil
   min_number = nil
@@ -11,7 +13,11 @@ def key_for_min_value(jerseys)
       if min_name == nil || number < min_number
         min_name = name
         min_number = number
+        binding.pry
       end #if
     end #jersey
     min_name
 end #key_for_min_value
+
+jerseys = {:Becky => 23, :Emirchmp => 42, :Amanda => 21, :Daniel => 3}
+key_for_min_value(jerseys)
